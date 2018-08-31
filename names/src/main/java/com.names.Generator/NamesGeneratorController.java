657ef09258833e7f36be207b2a7ccb4f92171a7e
Viktoria Mezei <viktoria.mezei0708@gmail.com>
2018-08-31 09:38:42 +0100

    localhost loads without adding home to url

--- names/src/main/java/com.names.Generator/NamesGeneratorController.java
+++ names/src/main/java/com.names.Generator/NamesGeneratorController.java
@@ -3,10 +3,20 @@ package com.names.Generator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class NamesGeneratorController {
+
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        // any processing
+
+        //userRepository.findById(1);
+        return "home";
+    }
 
     @GetMapping("/home")
     public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
