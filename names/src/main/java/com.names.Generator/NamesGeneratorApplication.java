aafb05b7c6a35b1b09047b987f7a4354b7334381
Viktoria Mezei <viktoria.mezei0708@gmail.com>
2018-08-31 09:17:56 +0100

    skeleton

---
+++ names/src/main/java/com.names.Generator/NamesGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.names.Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamesGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamesGeneratorApplication.class, args);
+    }
+
+}
